0acf0e681db6d340a73a7258989d71a3fac56608
Rekimart <elarasoft.dev@gmail.com>
2025-01-31 12:44:18 -0800

    Change style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 // Create an Express application
 const app = express();
@@ -14,41 +14,50 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cors());
 
 // Example route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 // Example POST route
-app.post('/get-data', async (req, res) => {
+app.post("/get-data", async (req, res) => {
   const data = req.body;
   const netuid = data.netuid || 57;
   const coldkey = data.coldkey;
-  axios.get(`https://taomarketcap.com/api/subnets/${netuid}/metagraph`)
-    .then(({data}) => {
-      let out = data.filter(item => item.coldkey === coldkey);
-      let result = out.reduce((acc, item) => {
-        acc.taoPerDay += Math.round(item.taoPerDay * 1000) / 1000;
-        acc.stake += Math.round(item.stake * 1000) / 1000;
-        acc.pendingEmission += Math.round(item.pendingEmission * 1000) / 1000;
-        return acc;
-      }, {taoPerDay: 0, stake: 0, pendingEmission: 0});
-
+  axios
+    .get(`https://taomarketcap.com/api/subnets/${netuid}/metagraph`)
+    .then(({ data }) => {
+      let out = data.filter((item) => item.coldkey === coldkey);
+      let result = out.reduce(
+        (acc, item) => {
+          acc.taoPerDay += item.taoPerDay;
+          acc.stake += item.stake;
+          acc.pendingEmission += item.pendingEmission;
+          return acc;
+        },
+        { taoPerDay: 0, stake: 0, pendingEmission: 0 }
+      );
+      const text = `🚨${netuid} ⛏️${out.length} 🔑${coldkey.substr(0, 7)} 🏠${
+        Math.round(result.stake * 1000) / 1000
+      } 🏅${Math.round(result.taoPerDay * 1000) / 1000} 🎯${
+        Math.round(result.pendingEmission * 1000) / 1000
+      }`;
       res.json({
-        text: `🚨${netuid} ⛏️${out.length} 🔑${coldkey.substr(0, 7)} 🏠${result.stake} 🏅${result.taoPerDay} 🎯${result.pendingEmission}`,
+        text,
         blocks: [
           {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `🚨${netuid} ⛏️${out.length} 🔑${coldkey.substr(0, 7)} 🏠${result.stake} 🏅${result.taoPerDay} 🎯${result.pendingEmission}`
-            }
-          }
-        ]
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text,
+            },
+          },
+        ],
       });
-    }).catch(error => {
+    })
+    .catch((error) => {
       console.log(error);
       res.json({ text: error });
-    })
+    });
 });
 
 // Start the server
